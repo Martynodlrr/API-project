889779614f391242831794a3762e4d651cc9e7e3
Martyn <martynodlrr@gmail.com>
2023-07-11 14:30:40 -0500

    post spots completed

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -41,51 +41,28 @@ router.get('/:spotId', async (req, res) => {
 router.post('/', async (req, res) => {
     const { user } = req;
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
-    const errors = {};
     let spot;
-
-    if (!address) {
-        errors.address = "Street address is required"
-    }
-    if (!city) {
-        errors.city = "City is required"
-    }
-    if (!state) {
-        errors.state = "State is required"
-    }
-    if (!country) {
-        errors.country = "Country is required"
-    }
-    if (!lat) {
-        errors.lat = "Latitude is not valid"
-    }
-    if (!lng) {
-        errors.lng = "Longitude is not valid"
-    }
-    if (!name) {
-        errors.name = "Name must be less than 50 characters"
-    }
-    if (!description) {
-        errors.description = "Description is required"
-    }
-    if (!price) {
-        errors.price = "Price per day is required"
-    }
-
-    const length = Object.keys(errors).length;
 
     if (user) {
         try {
             spot = await Spot.create({ ownerId: user.id, address, city, state, country, lat, lng, name, description, price });
             return res.status(201).json(spot);
-          } catch (error) {
-            if (error instanceof Sequelize.ValidationError) {
-              return res.status(400).json({ error, errors });
+        } catch (err) {
+            const errors = [];
+
+            for (const key in err.errors) {
+                if (err.errors[key].message.startsWith('Spot')) {
+                    errors.push(err.errors[key].message.slice(5));
+                } else {
+                    errors.push(err.errors[key].message);
+                }
             }
-            return res.status(400).json({ "message": "Bad Request", errors });
-          }
+
+            return res.status(400).json({ errors });
+        }
     }
-    res.json()
+
+    return res.status(401).json({ "message": "Invalid credentials" });
 });
 
 module.exports = router;

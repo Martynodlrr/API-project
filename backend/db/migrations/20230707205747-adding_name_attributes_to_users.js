bc17262031d9e1044ab1fda3abe1f14a8d03fa05
Martyn <martynodlrr@gmail.com>
2023-07-13 17:17:57 -0500

    refactoring the migration files

---
+++ backend/db/migrations/20230707205747-adding_name_attributes_to_users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // Perform the migration
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // Undo the migration
+  }
+};
